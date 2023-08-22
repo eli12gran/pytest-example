3e3c754a0e71c41ec7d690bd2ea800c3f97da369
Elizabeth <elizabethgran12@gmail.com>
2023-08-22 14:57:18 -0500

    holaaa

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a,b):
+  return a+b
+
+def substract(a,b):
+  return a+b
+
+def multiply(a,b):
+  return a*b
+
+def convert_farenheintToCelsius(farenheit):
+  return multiply(substract(farenheit, 32), 9/5)
---
+++ test_functions.py
@@ -0,0 +1,17 @@
+from functions import add, substract, multiply
+from functions import convert_farenheintToCelsius as f2c
+
+def test_add():
+  assert add(2,3) == 5
+  assert add("space", "ship") == "spaceship"
+
+#uncomment the following test in step 5
+
+#def test_substract():
+#  assert substract(2,3) == -1
+
+#def test_convert_farenheintToCelsius():
+#  assert f2c(32) == 0
+#  assert f2c(122) == pytest.approx(50)
+#  with pytest.raises(AssertionError):
+#    f2c(-600)
