40958f6d3dd129c40dc701e08d3a97448c268f10
Elizabeth <elizabethgran12@gmail.com>
2023-08-22 15:06:07 -0500

    descomentar

--- test_functions.py
+++ test_functions.py
@@ -7,11 +7,11 @@ def test_add():
 
 #uncomment the following test in step 5
 
-#def test_substract():
-#  assert substract(2,3) == -1
+def test_substract():
+  assert substract(2,3) == -1
 
-#def test_convert_farenheintToCelsius():
-#  assert f2c(32) == 0
-#  assert f2c(122) == pytest.approx(50)
-#  with pytest.raises(AssertionError):
-#    f2c(-600)
+def test_convert_farenheintToCelsius():
+  assert f2c(32) == 0
+  assert f2c(122) == pytest.approx(50)
+  with pytest.raises(AssertionError):
+    f2c(-600)
